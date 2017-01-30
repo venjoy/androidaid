ea22568049c0c818a7e16279674e1aa7f2b457cb
Vaibhav Jain <jainvaibhav35@gmail.com>
2017-01-31 00:12:32 +0530

    Login Interface

---
+++ app/src/main/java/com/venjoy/socialogin/appinterface/LoginInterface.java
@@ -0,0 +1,15 @@
+package com.venjoy.socialogin.appinterface;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import socialogin.venjoy.com.socialogin.R;
+
+/**
+ * Created by Vaibhav on 30-01-2017.
+ */
+
+public interface LoginInterface{
+
+}
